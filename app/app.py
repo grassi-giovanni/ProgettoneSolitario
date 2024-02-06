ab4286b0d08b8b75cbbc0022954b2a5b680e04c1
grassi-giovanni <grassi.giovanni@iisgalvanimi.edu.it>
2024-02-06 10:43:39 +0000

    miINcazzo

--- app/app.py
+++ app/app.py
@@ -28,8 +28,8 @@ class Applicazione  :
     collection_names = db.list_collection_names()
     print(f"Collezioni nel database '{database_name}': {collection_names}")
 
-@app.route('/simple_json')
-def simple_json():
+@app.route('/ottieni_All')
+def ottieni_All():
     uri = "mongodb+srv://grassigiovanni:giogalvani@cluster0.8hojujv.mongodb.net/?retryWrites=true&w=majority"
     # Create a new client and connect to the server
     client = MongoClient(uri, server_api=ServerApi('1'))
@@ -45,6 +45,40 @@ def simple_json():
     print (output)
 
     return jsonify({'DATI OTTENUTI': output})
+
+
+@app.route('/inserisci')
+def inserisci():
+    uri = "mongodb+srv://grassigiovanni:giogalvani@cluster0.8hojujv.mongodb.net/?retryWrites=true&w=majority"
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
+   
+    mydb = client["TopUni"]
+    mycol = mydb["uniList"]
+
+
+    data=( {
+        "sequence": 2,
+        "rank": 3,
+        "university": "Milano",
+        "overall_score": 98.9,
+        "academic_reputation": 100,
+        "employer_reputation": 100,
+        "faculty_student_ratio": 100,
+        "citations_per_faculty": 90.6,
+        "international_faculty_ratio": 98.2,
+        "international_students_ratio": 98.2,
+        "international_research_network": 100,
+        "employment_outcomes": 100,
+        "sustainability": 97.8,
+        "Fundos (US$)": 6.7
+    })
+
+
+    result = mycol.insert_one(data)
+ 
+    return  {"status": "success", "data": "Inserita correttamente", "message": "dati insrit"}
+
 
 
 if __name__=='__main__':
